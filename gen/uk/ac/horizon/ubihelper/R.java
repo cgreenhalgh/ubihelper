86921c2fa50441ec5eacb5e66a2fd93e60b80944
Chris Greenhalgh <cmg@cs.nott.ac.uk>
2012-02-21 23:34:24 +0000

    skeleton activities for peer list and search list

--- gen/uk/ac/horizon/ubihelper/R.java
+++ gen/uk/ac/horizon/ubihelper/R.java
@@ -15,24 +15,27 @@ public final class R {
         public static final int notification_icon=0x7f020001;
     }
     public static final class id {
-        public static final int linearLayout1=0x7f070002;
-        public static final int test_button=0x7f070000;
-        public static final int test_text=0x7f070001;
-        public static final int wifi_gateway_text=0x7f070009;
-        public static final int wifi_ip2_text=0x7f070007;
-        public static final int wifi_ip_text=0x7f070005;
-        public static final int wifi_mac_text=0x7f070006;
-        public static final int wifi_netmask_text=0x7f070008;
-        public static final int wifi_ssid_text=0x7f070004;
-        public static final int wifi_status_text=0x7f070003;
-        public static final int wifistatus_option=0x7f07000a;
+        public static final int linearLayout1=0x7f080002;
+        public static final int test_button=0x7f080000;
+        public static final int test_text=0x7f080001;
+        public static final int wifi_gateway_text=0x7f080009;
+        public static final int wifi_ip2_text=0x7f080007;
+        public static final int wifi_ip_text=0x7f080005;
+        public static final int wifi_mac_text=0x7f080006;
+        public static final int wifi_netmask_text=0x7f080008;
+        public static final int wifi_ssid_text=0x7f080004;
+        public static final int wifi_status_text=0x7f080003;
+        public static final int wifistatus_option=0x7f08000a;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int wifistatus=0x7f030001;
+        public static final int add_peer_item=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int peer_item=0x7f030002;
+        public static final int search_peer_item=0x7f030003;
+        public static final int wifistatus=0x7f030004;
     }
     public static final class menu {
-        public static final int mainpreferencesmenu=0x7f060000;
+        public static final int mainpreferencesmenu=0x7f070000;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
@@ -52,6 +55,10 @@ public final class R {
         public static final int wifidisc_preference_title=0x7f05000e;
         public static final int wifidisc_preferences=0x7f05000d;
         public static final int wifistatus_option_title=0x7f050010;
+    }
+    public static final class style {
+        public static final int list_item_title=0x7f060000;
+        public static final int list_small_header=0x7f060001;
     }
     public static final class xml {
         public static final int mainpreferences=0x7f040000;
--- src/uk/ac/horizon/ubihelper/SearchPeersActivity.java
+++ src/uk/ac/horizon/ubihelper/SearchPeersActivity.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.ListActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -24,7 +23,7 @@ import android.widget.Toast;
  * @author cmg
  *
  */
-public class ManagePeersActivity extends ListActivity {
+public class SearchPeersActivity extends ListActivity {
 	private View searchView;
 	
 	@Override
@@ -32,18 +31,18 @@ public class ManagePeersActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		// TODO from table
 		ArrayAdapter<String> aa = new ArrayAdapter<String>(this, R.layout.peer_item);
-		aa.add("Hello list");
+		aa.add("Hello list 2");
 		ListView lv = getListView();
 		// following line fails with addView not supported in AdapterView
-		searchView = getLayoutInflater().inflate(R.layout.add_peer_item, null);
+		searchView = getLayoutInflater().inflate(R.layout.search_peer_item, null);
+		((TextView)searchView).setText("Searching...");
 		lv.addHeaderView(searchView, "Add a new item", true);
 		lv.setAdapter(aa);
 		
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				if (view==searchView) {
-					Intent i = new Intent(ManagePeersActivity.this, SearchPeersActivity.class);
-					startActivity(i);
+					
 				}
 				else {
 					Toast.makeText(getApplicationContext(), ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
